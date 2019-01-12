f329db9a75883a6ab5bd891f70f74c62316fc56d
Parallels <96pengpeng@gmail.com>
2019-01-12 14:41:46 +0800

    signin.php finished

--- php/signup.php
+++ php/signup.php
@@ -8,8 +8,8 @@ $pwd = $_POST['password'];
 $province = $_POST['province'];
 $city = $_POST['city'];
 $gender = $_POST['gender'];
-if($gender == 'male')	$gender = 'm';
-if($gender == 'female')	$gender = 'f';
+if($gender == 'male')	$gender = "m";
+if($gender == 'female')	$gender = "f";
 
 $username = "root";
 $password = "123456";
@@ -20,22 +20,16 @@ $gc = new GstoreConnector("127.0.0.1", 9000);
 $flag = 1;
 while($flag == 1){
 	$uid = rand(1000000000, 9999999999);
-	$if_uid_exist = 'select (COUNT(?uid) as ?uidcnt) where{ ?uid <exist> <user:' . $uid . '> .}';
+	$if_uid_exist = "select (COUNT(?uid) as ?uidcnt) where\n{\n?uid <exist> <user/" . $uid . "> .\n}";
 	$query = $gc->query($username, $password, "weibo", $if_uid_exist);
 	$obj = json_decode($query);
+	// echo $query;
 	if(count($obj->results->bindings) == 0)
 		$flag = 0;
 }
-// echo $uid . PHP_EOL;
-// echo $name . PHP_EOL;
-// echo $screenname . PHP_EOL;
-// echo $pwd . PHP_EOL;
-// echo $gender . PHP_EOL;
 
 // url
 $url = "http://localhost/homepage.php?suid=" . $uid;
-// echo $url . PHP_EOL;
-
 $createdat = date('Y-m-d H:i:s');
 $friendsnum = 0;
 $followersnum = 0;
@@ -43,40 +37,41 @@ $statusesnum = 0;
 $favouritesnum = 0;
 $location = $province . " " . $city;
 
+$useridentity = "<user/" . $uid . ">";
+$insertuid = "<useridentity> <exist> <user/" . $uid . "> .\n";
+$insertname = $useridentity . " <has_name> \"" . $name . "\" .\n";
+$insertsname = $useridentity . " <has_screen_name> \"" . $screenname . "\" .\n";
+$insertloc = $useridentity . " <has_location> \"" . $location . "\" .\n";
+$inserturl = $useridentity . " <has_url> \"" . $url . "\" .\n";
+$insertgender = $useridentity . " <has_gender> \"" . $gender . "\" .\n";
+$insertfollowersnum = $useridentity . " <has_followersnum> " . $followersnum . " .\n";
+$insertfriendsnum = $useridentity . " <has_friendsnum> " . $friendsnum . " .\n";
+$insertstatusesnum = $useridentity . " <has_statusesnum> " . $statusesnum . " .\n";
+$insertfavouritesnum = $useridentity . " <has_favouritesnum> " . $favouritesnum . " .\n";
+$inserttime = $useridentity . " <created_at> \"" . $createdat . "\" .\n";
+$insertpwd = $useridentity . " <has_password> \"" . $pwd . "\" .\n";
 
-// echo $createdat . PHP_EOL;
-// echo $friendsnum . PHP_EOL;
-// echo $followersnum . PHP_EOL;
-// echo $statusesnum . PHP_EOL;
-// echo $favouritesnum . PHP_EOL;
-// echo $location . PHP_EOL;
+$insertdata = "insert data\n{\n" . $insertuid . $insertname . $insertsname . $insertloc . $inserturl . $insertgender . $insertfollowersnum . $insertfriendsnum . $insertstatusesnum . $insertfavouritesnum . $inserttime . $insertpwd . "}";
 
-$insertuid = '<useridentity> <exist> <user:' . $uid . '> .' . PHP_EOL;
-$insertname = '<user:' . $uid . '> <has_name> "' . $name . '" .' . PHP_EOL;
-$insertsname = '<user:' . $uid . '> <has_screen_name> "' . $screenname . '" .' . PHP_EOL;
-$insertloc = '<user:' . $uid . '> <has_location> "' . $location . '" .' . PHP_EOL;
-$inserturl = '<user:' . $uid . '> <has_url> "' . $url . '" .' . PHP_EOL;
-$insertgender = '<user:' . $uid . '> <has_gender> "' . $gender . '" .' . PHP_EOL; 
-$insertfollowersnum = '<user:' . $uid . '> <has_followersnum> ' . $followersnum . ' .' . PHP_EOL;
-$insertfriendsnum = '<user:' . $uid . '> <has_friendsnum> ' . $friendsnum . ' .' . PHP_EOL;
-$insertstatusesnum = '<user:' . $uid . '> <has_statusesnum> ' . $statusesnum . ' .' . PHP_EOL;
-$insertfavouritesnum = '<user:' . $uid . '> <has_favouritesnum> ' . $favouritesnum . ' .' . PHP_EOL;
-$inserttime = '<user:' . $uid . '> <created_at> "' . $createdat . '" .' . PHP_EOL;
-$insertpwd = '<user:' . $uid . '> <has_password> "' . $pwd . '" .';
-
-$insertdata = 'insert data { ' . PHP_EOL . $insertuid . $insertname . $insertsname . $insertloc . $inserturl . $insertgender . $insertfollowersnum . $insertfriendsnum . $insertstatusesnum . $insertfavouritesnum . $inserttime . $insertpwd . '}';
-
-echo $insertdata . PHP_EOL;
+// echo $insertdata . PHP_EOL;
 
 $res = $gc->query($username, $password, "weibo", $insertdata);
+// echo $res . PHP_EOL;
 $obj = json_decode($res);
 
-$arr = array('status' => 'false');
-$ret = json_encode($arr);
-if($obj->StatusMsg == 'update query returns true.'){
-	$arr = array('status' => 'true', 'uid' => $uid);
-	$ret = json_encode($arr);
+$arr = array("status" => "false");
+if($obj->StatusMsg == "update query returns true."){
+	$check = "select ?x where\n{\n" . $useridentity . " ?y ?x .\n}";
+	// echo $check . PHP_EOL;
+	$checkres = $gc->query($username, $password, "weibo", $check);
+	$checkobj = json_decode($checkres);
+	// echo $checkres . PHP_EOL;
+	// echo count($checkobj->results->bindings) . PHP_EOL;
+	if(count($checkobj->results->bindings) == 11){
+		$arr = array('status' => 'true', 'uid' => $uid);
+	}
 }
+$ret = json_encode($arr);
 echo $ret;
 
 ?>
