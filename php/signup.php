fc406b20ac9db99a13156b6c95cf3eaf06e764ef
Parallels <96pengpeng@gmail.com>
2019-01-12 11:37:12 +0800

    signin

--- php/signup.php
+++ php/signup.php
@@ -8,12 +8,12 @@ $pwd = $_POST['password'];
 $province = $_POST['province'];
 $city = $_POST['city'];
 $gender = $_POST['gender'];
+if($gender == 'male')	$gender = 'm';
+if($gender == 'female')	$gender = 'f';
 
 $username = "root";
 $password = "123456";
 $gc = new GstoreConnector("127.0.0.1", 9000);
-//$ret = $gc->build("weibo", "data/weibo/weibo.nt", $username, $password); 
-//$ret = $gc->load("weibo", $username, $password);
 
 
 // find a new uid
@@ -26,15 +26,15 @@ while($flag == 1){
 	if(count($obj->results->bindings) == 0)
 		$flag = 0;
 }
-echo $uid . PHP_EOL;
-echo $name . PHP_EOL;
-echo $screenname . PHP_EOL;
-echo $pwd . PHP_EOL;
-echo $gender . PHP_EOL;
+// echo $uid . PHP_EOL;
+// echo $name . PHP_EOL;
+// echo $screenname . PHP_EOL;
+// echo $pwd . PHP_EOL;
+// echo $gender . PHP_EOL;
 
 // url
 $url = "http://localhost/homepage.php?suid=" . $uid;
-echo $url . PHP_EOL;
+// echo $url . PHP_EOL;
 
 $createdat = date('Y-m-d H:i:s');
 $friendsnum = 0;
@@ -44,10 +44,39 @@ $favouritesnum = 0;
 $location = $province . " " . $city;
 
 
-echo $createdat . PHP_EOL;
-echo $friendsnum . PHP_EOL;
-echo $followersnum . PHP_EOL;
-echo $statusesnum . PHP_EOL;
-echo $favouritesnum . PHP_EOL;
-echo $location . PHP_EOL;
+// echo $createdat . PHP_EOL;
+// echo $friendsnum . PHP_EOL;
+// echo $followersnum . PHP_EOL;
+// echo $statusesnum . PHP_EOL;
+// echo $favouritesnum . PHP_EOL;
+// echo $location . PHP_EOL;
+
+$insertuid = '<useridentity> <exist> <user:' . $uid . '> .' . PHP_EOL;
+$insertname = '<user:' . $uid . '> <has_name> "' . $name . '" .' . PHP_EOL;
+$insertsname = '<user:' . $uid . '> <has_screen_name> "' . $screenname . '" .' . PHP_EOL;
+$insertloc = '<user:' . $uid . '> <has_location> "' . $location . '" .' . PHP_EOL;
+$inserturl = '<user:' . $uid . '> <has_url> "' . $url . '" .' . PHP_EOL;
+$insertgender = '<user:' . $uid . '> <has_gender> "' . $gender . '" .' . PHP_EOL; 
+$insertfollowersnum = '<user:' . $uid . '> <has_followersnum> ' . $followersnum . ' .' . PHP_EOL;
+$insertfriendsnum = '<user:' . $uid . '> <has_friendsnum> ' . $friendsnum . ' .' . PHP_EOL;
+$insertstatusesnum = '<user:' . $uid . '> <has_statusesnum> ' . $statusesnum . ' .' . PHP_EOL;
+$insertfavouritesnum = '<user:' . $uid . '> <has_favouritesnum> ' . $favouritesnum . ' .' . PHP_EOL;
+$inserttime = '<user:' . $uid . '> <created_at> "' . $createdat . '" .' . PHP_EOL;
+$insertpwd = '<user:' . $uid . '> <has_password> "' . $pwd . '" .';
+
+$insertdata = 'insert data { ' . PHP_EOL . $insertuid . $insertname . $insertsname . $insertloc . $inserturl . $insertgender . $insertfollowersnum . $insertfriendsnum . $insertstatusesnum . $insertfavouritesnum . $inserttime . $insertpwd . '}';
+
+echo $insertdata . PHP_EOL;
+
+$res = $gc->query($username, $password, "weibo", $insertdata);
+$obj = json_decode($res);
+
+$arr = array('status' => 'false');
+$ret = json_encode($arr);
+if($obj->StatusMsg == 'update query returns true.'){
+	$arr = array('status' => 'true', 'uid' => $uid);
+	$ret = json_encode($arr);
+}
+echo $ret;
+
 ?>
