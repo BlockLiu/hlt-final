2355720b9974e7fc38427cd73f7d8c54baf748b2
Parallels <96pengpeng@gmail.com>
2019-01-13 14:07:01 +0800

    publish.php

--- php/publish.php
+++ php/publish.php
@@ -1,13 +1,13 @@
-<<?php
+<?php
 
 require "GstoreConnector.php";
 
 $uid = $_POST['uid'];
-$startidx = $_POST['start'];
-$len = $_POST['len'];
-// $uid = $_GET['uid'];
-// $startidx = $_GET['start'];
-// $len = $_GET['len'];
+$topic = $_POST['topic'];
+$source = $_POST['source'];
+$text = $_POST['text'];
+$picture = $_POST['picture'];
+
 
 $username = "root";
 $password = "123456";
@@ -25,59 +25,62 @@ if(count($obj->results->bindings) == 0){
 	return;
 }
 
-/*
- select ?wid ?time where
- {
-	$uid <follow> ?tuid .
-	?wid <from_uid> ?tuid .
-	?wid <publish_date> ?time .
- } ORDER BY DESC(?time) LIMIT $len OFFSET $startidx
- */
-$weibotimequery = "select ?wid ?time where\n{\n" . $useridentity . " <follow> ?tuid .\n?wid <from_uid> ?tuid .\n?wid <publish_date> ?time .\n} ORDER BY DESC(?time) LIMIT " . $len . " OFFSET " . $startidx;
-$query = $gc->query($username, $password, "weibo", $weibotimequery);
-
-$obj = json_decode($query);
-$widset = $obj->results->bindings;
-$arr = array();
-for($i = 0; $i < count($widset); $i++)
-{
-
-	$widentity = "<" . $widset[$i]->wid->value . ">";
-	$time = $widentity . " <publish_date> ?time .\n";
-	$repostsnum = $widentity . " <repostsnum> ?repostsnum .\n";
-	$commentsnum = $widentity . " <commentsnum> ?commentsnum .\n";
-	$attitudesnum = $widentity . " <attitudesnum> ?attitudesnum .\n";
-	$fromuid = $widentity . " <from_uid> ?fromuid .\n";
-	$topic = $widentity . " <topic> ?topic .\n";
-	$source = $widentity . " <source> ?source .\n";
-	$text = $widentity . " <text> ?text .\n";
-	$weiboquery = "select ?time ?repostsnum ?commentsnum ?attitudesnum ?fromuid ?topic ?source ?text where\n{\n" . $time . $repostsnum . $commentsnum . $attitudesnum . $fromuid . $topic . $source . $text . "}";
-	// echo $weiboquery . PHP_EOL;
-	$weiboinfo = $gc->query($username, $password, "weibo", $weiboquery);
-	// echo $weiboinfo . PHP_EOL;
-	$obj = json_decode($weiboinfo);
 
 
-	$snamequery = "select ?sname where\n{\n" . $obj->results->bindings[0]->fromuid->value . " <has_screen_name> ?sname .\n}";
-	$snameinfo = $gc->query($username, $password, "weibo", $snameinfo);
-	$obj2 = json_decode($snameinfo);
-	$sname = $obj2->results->bindings[0]->sname->value;
-
-	$a = array("wid"=>substr($widset[$i]->wid->value, 6),
-			   "publish_time"=>$obj->results->bindings[0]->time->value,
-			   "repostsnum"=>$obj->results->bindings[0]->repostsnum->value,
-			   "commentsnum"=>$obj->results->bindings[0]->commentsnum->value,
-			   "attitudesnum"=>$obj->results->bindings[0]->attitudesnum->value,
-			   "fromuid"=>substr($obj->results->bindings[0]->fromuid->value, 5),
-			   "topic"=>$obj->results->bindings[0]->topic->value,
-			   "source"=>$obj->results->bindings[0]->source->value,
-			   "text"=>$obj->results->bindings[0]->text->value,
-			   "sname"=>$sname);
-	$arr[] = $a;
+// find a new wid
+$flag = 1;
+while($flag == 1){
+	$wid = rand(1000000000000000, 9999999999999999);
+	$if_wid_exist = "select (COUNT(?wid) as ?widcnt) where\n{\n?wid <exist> <weibo/" . $wid . "> .\n}";
+	$query = $gc->query($username, $password, "weibo", $if_wid_exist);
+	$obj = json_decode($query);
+	// echo $query;
+	if(count($obj->results->bindings) == 0)
+		$flag = 0;
 }
+$weiboidentity = "<weibo/" . $wid . ">";
+$insertdata = "insert data\n{\n<weiboidentity> <exist> " . $weiboidentity . " .\n}";
+// echo $insertdata . PHP_EOL;
+$res = $gc->query($username, $password, "weibo", $insertdata);
+// echo $res . PHP_EOL;
 
-$retarr = array("weibo_num"=>count($widset), "content"=>$arr);
-$ret = json_encode($retarr);
+
+
+$publish_date = date('Y-m-d H:i:s');
+$repostsnum = "0";
+$commentsnum = "0";
+$attitudesnum = "0";
+
+
+$insertdate = $weiboidentity . " <publish_date> \"" . $publish_date . "\" ;\n";
+$insertrep = "<repostsnum> \"" . $repostsnum . "\" ;\n";
+$insertcom = "<commentsnum> \"" . $commentsnum . "\" ;\n";
+$insertatt = "<attitudesnum> \"" . $attitudesnum . "\" ;\n";
+$insertuid = "<from_uid> " . $useridentity . " ;\n";
+$inserttopic = "<topic> \"" . $topic . "\" ;\n";
+$insertsource = "<source> \"" . $source . "\" ;\n";
+$inserttext = "<text> \"" . $text . "\" ;\n";
+$insertpic = "<picture> \"" . $picture . "\" .\n";
+
+$insertdata = "insert data\n{\n" . $insertdate . $insertrep . $insertcom . $insertatt . $insertuid . $inserttopic . $insertsource . $inserttext . $insertpic . "}";
+$res = $gc->query($username, $password, "weibo", $insertdata);
+
+$obj = json_decode($res);
+
+
+$arr = array("status" => "fail");
+if($obj->StatusMsg == "update query returns true."){
+	$check = "select ?x where\n{\n" . $weiboidentity . " ?y ?x .\n}";
+	// echo $check . PHP_EOL;
+	$checkres = $gc->query($username, $password, "weibo", $check);
+	$checkobj = json_decode($checkres);
+	echo $checkres . PHP_EOL;
+	// echo count($checkobj->results->bindings) . PHP_EOL;
+	if(count($checkobj->results->bindings) == 9){
+		$arr = array('status' => 'success', 'wid' => $wid);
+	}
+}
+$ret = json_encode($arr);
 echo $ret;
 
 ?>
