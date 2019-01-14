d1a20e16ad59013dc70c1adec9531facfca5c872
Parallels <96pengpeng@gmail.com>
2019-01-14 11:31:34 +0800

    pulish.php

--- php/publish.php
+++ php/publish.php
@@ -5,13 +5,22 @@ $topic = $_POST['topic'];
 $source = $_POST['source'];
 $text = $_POST['text'];
 $picture = $_POST['picture'];
+
+echo $source . PHP_EOL . $text . PHP_EOL;
+$source = str_replace('"', '\"', $source);
+$text = str_replace('"', '\"', $text);
+echo $source . PHP_EOL . $text . PHP_EOL;
+
 $username = "root";
 $password = "123456";
 $gc = new GstoreConnector("127.0.0.1", 9000);
+
+
+
 $useridentity = "<user/" . $uid . ">";
 $if_uid_exist = "select (COUNT(?uid) as ?uidcnt) where\n{\n?uid <exist> " . $useridentity . " .\n}";
-//echo $if_uid_exist . PHP_EOL;
 $query = $gc->query($username, $password, "weibo", $if_uid_exist);
+// echo $if_uid_exist . PHP_EOL . $query . PHP_EOL;
 $obj = json_decode($query);
 if(count($obj->results->bindings) == 0){
 	$arr = array("status" => "no such uid");
@@ -30,26 +39,53 @@ while($flag == 1){
 	if(count($obj->results->bindings) == 0)
 		$flag = 0;
 }
+
+
+
+
 $weiboidentity = "<weibo/" . $wid . ">";
 $insertdata = "insert data\n{\n<weiboidentity> <exist> " . $weiboidentity . " .\n}";
 // echo $insertdata . PHP_EOL;
 $res = $gc->query($username, $password, "weibo", $insertdata);
 // echo $res . PHP_EOL;
+
+
+$if_wid_exist = "select (COUNT(?wid) as ?widcnt) where\n{\n?wid <exist> <weibo/" . $wid . "> .\n}";
+$query = $gc->query($username, $password, "weibo", $if_wid_exist);
+$obj = json_decode($query);
+// echo $query;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $publish_date = date('Y-m-d H:i:s');
 $repostsnum = "0";
 $commentsnum = "0";
 $attitudesnum = "0";
-$insertdate = $weiboidentity . " <publish_date> \"" . $publish_date . "\" ;\n";
-$insertrep = "<repostsnum> \"" . $repostsnum . "\" ;\n";
-$insertcom = "<commentsnum> \"" . $commentsnum . "\" ;\n";
-$insertatt = "<attitudesnum> \"" . $attitudesnum . "\" ;\n";
-$insertuid = "<from_uid> " . $useridentity . " ;\n";
-$inserttopic = "<topic> \"" . $topic . "\" ;\n";
-$insertsource = "<source> \"" . $source . "\" ;\n";
-$inserttext = "<text> \"" . $text . "\" ;\n";
-$insertpic = "<picture> \"" . $picture . "\" .\n";
+$insertdate = $weiboidentity . " <publish_date> \"" . $publish_date . "\" .\n";
+$insertrep = $weiboidentity . " <repostsnum> \"" . $repostsnum . "\" .\n";
+$insertcom = $weiboidentity . " <commentsnum> \"" . $commentsnum . "\" .\n";
+$insertatt = $weiboidentity . " <attitudesnum> \"" . $attitudesnum . "\" .\n";
+$insertuid = $weiboidentity . " <from_uid> " . $useridentity . " .\n";
+$inserttopic = $weiboidentity . " <topic> \"" . $topic . "\" .\n";
+$insertsource = $weiboidentity . " <source> \"" . $source . "\" .\n";
+$inserttext = $weiboidentity . " <text> \"" . $text . "\" .\n";
+$insertpic = $weiboidentity . " <picture> \"" . $picture . "\" .\n";
 $insertdata = "insert data\n{\n" . $insertdate . $insertrep . $insertcom . $insertatt . $insertuid . $inserttopic . $insertsource . $inserttext . $insertpic . "}";
+// $insertdata = "insert data\n{\n" . $insertsource . "}";
 $res = $gc->query($username, $password, "weibo", $insertdata);
+echo $insertdata . PHP_EOL . $res . PHP_EOL;
 $obj = json_decode($res);
 $arr = array("status" => "fail");
 if($obj->StatusMsg == "update query returns true."){
